94cb0e00140aa7b8932c4b26376b4f900d5237e9
Jesus17032003 <jvabrilm01@iesalbarregas.es>
2024-12-05 08:52:03 +0100

    PROXY.sh

--- PROXY.sh
+++ PROXY.sh
@@ -6,7 +6,7 @@ a2enmod proxy_balancer
 a2enmod lbmethod_byrequests
 cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/load-balancer.conf
 sed -i '/DocumentRoot \/var\/www\/html/s/^/#/' /etc/apache2/sites-available/load-balancer.conf
-sed -i '/:warn/ a \<Proxy balancer://mycluster>\n    # Server 1\n    BalancerMember http://192.168.10.133\n    # Server 2\n    BalancerMember http://>
+sed -i '/:warn/ a \<Proxy balancer://mycluster>\n    # Server 1\n    BalancerMember http://192.168.10.133\n    # Server 2\n    BalancerMember http://192.168.10.140\n</Proxy>\n#todas las peticiones las envía al siguiente balanceador\nProxyPass / balancer://mycluster/' /etc/apache2/sites-available/load-balancer.conf
 a2ensite load-balancer.conf
 a2dissite 000-default.conf
 systemctl restart apache2
